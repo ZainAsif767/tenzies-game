e2373e4d4ec08891d98e6a585315dbed5e6c0332
zainasif767 <zainclasher2002@gmail.com>
2023-01-11 17:18:09 +0500

    Feat: Added Ternary to set the background color

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,12 @@
-import { getValue } from "@testing-library/user-event/dist/utils";
 import React from "react";
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "wheat"
+
+    }
     return (
-        <div className="die-face">
+        <div className="die-face" style={styles}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
